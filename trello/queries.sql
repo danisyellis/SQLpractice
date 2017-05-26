fa405ab9b120115857e815b05db742075d0d96be
Danielle Gellis <danisyellis@gmail.com>
2017-05-26 15:17:00 -0700

    a couple more queries

--- trello/queries.sql
+++ trello/queries.sql
@@ -2,4 +2,5 @@ INSERT INTO users ("username", "password") VALUES ("dani", "password1");
 UPDATE users SET password="password2" WHERE username = "dani";
 DELETE FROM users WHERE id=3;
 
-SELECT * FROM 
+SELECT content FROM cards_in_column WHERE column_id = 3;
+SELECT content FROM cards_in_column WHERE column_id = 1 LIMIT 1;
--- twitter/queries.sql
+++ twitter/queries.sql
@@ -2,5 +2,5 @@ INSERT INTO tweets ("user_id", "content") VALUES (20, "My tweet is less than 140
 UPDATE tweets SET content="I hated that last tweet" WHERE id = 100;
 DELETE FROM users WHERE user_name = "stupid name";
 
-
-SELECT * FROM 
+SELECT content FROM tweets WHERE user_id = 3;
+SELECT content FROM tweets WHERE user_id = 1 LIMIT 1;
